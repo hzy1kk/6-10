3dc95a26bcb6ef68c19d2a1c9d0a0c887d81db67
hzy1kk <lucaslohan@colegiopaulodetarso.com.br>
2025-10-06 16:10:51 +0000

    save

--- 2.py
+++ 2.py
@@ -2,3 +2,4 @@ def soma (a,b):
     return a+b
 print (soma("mel","amor"))
 print (soma("15","-10"))
+print (soma(1,v19))
---
+++ 3.py
@@ -0,0 +1,13 @@
+valores = [
+    10,
+    3 + 2j,
+    5.5,
+    "amo minha namorada",
+    True,
+    False,
+    [15, 26, 10],
+    {"x": "valor"}
+]
+
+for valor in valores:
+    print(f"valor: {valor} - tipo: {type(valor)}")
